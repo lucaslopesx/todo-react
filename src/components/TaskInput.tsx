0be39c9229365f356f21a7eda2feeba79c8944de
lucaslopes <lucas.lopesx1@gmail.com>
2023-01-14 15:13:37 -0300

    feat: TaskInput component

--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -1,10 +1,11 @@
+import { PlusCircle } from 'phosphor-react'
 import styles from './TaskInput.module.css'
 
 export function TaskInput() {
   return (
     <div className={styles.taskInputContainer}>
-      <input type="text" />
-      <button>Criar</button>
+      <input type="text" placeholder='Adicione uma nova tarefa'/>
+      <button><strong>Criar</strong><PlusCircle size={20}/></button>
     </div>
   )  
 }
